4e993aac99f55c97d84646a3c64cba543379f8ec
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-01-29 16:53:31 +0900

    add text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hola, mundo¡"
+    render html: "hola, mundodddfkajfalj¡"
   end
 
   def goodbye
